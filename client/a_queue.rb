fc96db97a83debe1399cde4391e19f277de7a01e
maccman <maccman@4e43a301-793d-0410-b222-f1b7b3a1f9c5>
2008-01-09 00:25:55 +0000

    git-svn-id: http://sparrow.googlecode.com/svn/trunk@25 4e43a301-793d-0410-b222-f1b7b3a1f9c5

--- client/a_queue.rb
+++
@@ -1,7 +0,0 @@
-require 'socket'
-
-module AQueue
-  AQUEUE_ROOT = defined?(RAILS_ROOT) ? RAILS_ROOT : File.join(File.dirname(__FILE__))
-end
-  
-  
--- client/protocols.rb
+++
@@ -1,5 +0,0 @@
-module AQueue
-  module Protocols
-    class ConnectionError < StandardError; end #:nodoc:
-  end
-end
