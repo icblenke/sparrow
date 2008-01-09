2ed16430da74469b5b429e17fc6cb0f75a820c12
maccman <maccman@4e43a301-793d-0410-b222-f1b7b3a1f9c5>
2008-01-09 18:38:16 +0000

    git-svn-id: http://sparrow.googlecode.com/svn/trunk@31 4e43a301-793d-0410-b222-f1b7b3a1f9c5

--- client/m_queue/init.rb
+++ client/m_queue/init.rb
@@ -4,4 +4,10 @@ require File.join(File.dirname(__FILE__), 'lib', 'm_queue')
 require File.join(File.dirname(__FILE__), 'lib', 'protocols')
 require File.join(File.dirname(__FILE__), 'lib', 'queue')
 
-Dir.glob(File.join(File.dirname(__FILE__), 'lib', 'protocols', '*.rb')) {|i| require i rescue nil }
+Dir.glob(File.join(File.dirname(__FILE__), 'lib', 'protocols', '*.rb')) {|i| 
+  begin
+    require i
+  rescue MissingSourceFile
+   # Might not have sqs/beanstalk-client
+  end
+}
